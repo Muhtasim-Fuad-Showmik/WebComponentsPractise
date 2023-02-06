1d48ad94acb027c4cfe6b6346442b04ba71f6f21
Muhtasim-Fuad-Showmik <muhtasimfuadshowmik@gmail.com>
2023-02-06 10:51:40 +0600

    Created a web component card with basic CSS styles

--- fuad.js
+++ fuad.js
@@ -3,6 +3,7 @@ template.innerHTML = `
     <style>
         h2 {
             color: #023e8a;
+            font-weight: 700;
         }
 
         .light {
@@ -16,9 +17,60 @@ template.innerHTML = `
         .lightest {
             color: #caf0f8;
         }
+
+        #profile-picture {
+            width: 200px;
+            border-radius: 50%;
+            border: 8px solid white;
+            margin-top: 10px;
+        }
+
+        #text-details {
+            background-color: white;
+            padding: 30px;
+            border-radius: 8px;
+        }
+
+        .btn-primary {
+            min-width: 120px;
+            cursor: pointer;
+            background-color: #0077b6;
+            outline: none;
+            font-size: 16px;
+            color: white;
+            border: none;
+            border-radius: 4px;
+            padding: 10px 20px;
+            margin-top: 10px;
+        }
+
+        .user-info {
+        }
+
+        .user-profile {
+            background-color: #e3f8fc;
+            width: 700px;
+            min-height: 300px;
+            display: grid;
+            grid-template-columns: 1fr 2fr;
+            grid-gap: 20px;
+            margin-bottom: 20px;
+            border-radius: 8px;
+            padding: 30px;
+            vertical-align: middle;
+            box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+        }
     </style>
     <div class="user-profile">
-        <h2 id="user-name"></h2>
+        <img id="profile-picture" >
+        <div id="text-details">
+            <h2 id="user-name"></h2>
+            <div class="user-info">
+                <p>EMAIL</p>
+                <p>PHONE</p>
+            </div>
+            <button id="toggle-details" class="btn-primary" type="button">Less</button>
+        </div>
     </div>
 `;
 
@@ -29,6 +81,7 @@ class UserProfile extends HTMLElement {
         this.attachShadow({ mode: 'open' });
         this.shadowRoot.appendChild(template.content.cloneNode(true));
         this.shadowRoot.querySelector('#user-name').innerText = this.getAttribute('name');
+        this.shadowRoot.querySelector('#profile-picture').src = this.getAttribute('image');
     }
 }
 
